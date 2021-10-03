a3307a6fdab4b908d32d9e08ae6baaea158644b5
Andres Palma <palma.castaneda030@gmail.com>
2021-10-03 14:05:55 -0500

    Mapa actualizado

--- src/pages/Mapa.jsx
+++ src/pages/Mapa.jsx
@@ -1,45 +1,46 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import '../styles/mapa.css'
 
 const Mapa = () => {
 
     const [state, setstate] = useState({
-        longitude:0,
-        latitude:0
+        longitude: 0,
+        latitude: 0
     })
 
+    useEffect(() => {
+        handleUbicacion()
+    }, [])
     const handleUbicacion = () => {
         navigator.geolocation.getCurrentPosition(
             function (position) {
-                setstate({longitude:position.coords.longitude,
-                        latitude:position.coords.latitude})
+                setstate({
+                    longitude: position.coords.longitude,
+                    latitude: position.coords.latitude
+                })
             }
-            )
-        
+        )
+    }
+    return (
+        <div>
+            <button onClick={handleUbicacion}> Actualizar su ubicacion </button>
 
-    } 
-
-
-  return (
-    <div>
-        <button onClick ={handleUbicacion}> Ingresar ubicacion </button>
-
-      <div id="mapContainer">
-      <MapContainer center={[6.167, -75.333]} zoom={12} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-          <Marker position={[state.latitude,state.longitude]}>
-            <Popup>
-              Tu ubicacion
-            </Popup>
-          </Marker>
-      </MapContainer>
-      </div>
-    </div>
-  );
+            <div id="mapContainer">
+                <MapContainer center={[6.167, -75.333]} zoom={12} scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[state.latitude, state.longitude]}>
+                        <Popup>
+                            Tu ubicacion
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+        </div>
+    );
 };
 
 export default Mapa;
